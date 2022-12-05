57fcb5b3cc50a1ce46a7024468d38ebb015bfbdc
sararaa <84247798+sararaa@users.noreply.github.com>
2022-12-05 12:47:54 -0800

    Create Engine.cpp

---
+++ code/Engine.cpp
@@ -0,0 +1,66 @@
+#include "Engine.h"
+
+
+Engine::Engine()
+{
+	// Get the screen resolution and create an SFML window and View
+	Vector2f resolution;
+	resolution.x = VideoMode::getDesktopMode().width;
+	resolution.y = VideoMode::getDesktopMode().height;
+
+	m_Window.create(VideoMode(resolution.x, resolution.y),
+		"Singing Cats",
+		Style::Fullscreen);
+
+	// Initialize the full screen view
+	m_MainView.setSize(resolution);
+	m_HudView.reset(
+		FloatRect(0, 0, resolution.x, resolution.y));
+
+	// Inititialize the split-screen Views
+	m_LeftView.setViewport(
+		FloatRect(0.001f, 0.001f, 0.498f, 0.998f));
+
+	m_RightView.setViewport(
+		FloatRect(0.5f, 0.001f, 0.499f, 0.998f));
+
+	m_BGLeftView.setViewport(
+		FloatRect(0.001f, 0.001f, 0.498f, 0.998f));
+
+	m_BGRightView.setViewport(
+		FloatRect(0.5f, 0.001f, 0.499f, 0.998f));
+
+	// Can this graphics card use shaders?
+	if (!sf::Shader::isAvailable())
+	{
+		// Time to get a new PC
+		m_Window.close();
+	}
+
+	m_BackgroundTexture = TextureHolder::GetTexture(
+		"catbackstage/background.png");
+
+	// Associate the sprite with the texture
+	m_BackgroundSprite.setTexture(m_BackgroundTexture);
+
+}
+
+void Engine::run()
+{
+	// Timing 	
+	Clock clock;
+
+	while (m_Window.isOpen())
+	{
+		while (m_window.pollEvent(event))
+		{
+			if (event.key.code == Keyboard::Escape)
+			{
+				window.close();
+			}
+
+			if (event.type == Event::Closed) 
+			{ 
+				window.close(); 
+			}
+}
